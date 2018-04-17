ac2fcb78b35be837f06c94f59dc0c47bbfe52dda
Halyette <haly.lesane@gmail.com>
2018-04-17 14:48:11 +0000

    automatically backed up by learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,52 @@
+class TicTacToe
+  
+  attr_accessor :board
+  def initialize(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+    @board = board
+  end
+  
+    WIN_COMBINATIONS = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [6,4,2]
+      ]
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(string)
+    #binding.pry
+    string.to_i - 1
+  end
+  
+  def move(location, token)
+  board[location] = token
+ end
+
+ def position_taken?(index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(index)
+   index.between?(0,8) && !position_taken?(index)
+ end
+ 
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
+  
+   def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+ 
+end
